32951dbf24dd31ab051511f9d0f264ecd826dc00
Eric Tang <eric.tang22@gmail.com>
2026-02-12 14:55:22 -0800

    Multilevel prediction

--- model_predict.py
+++ model_predict.py
@@ -4,14 +4,14 @@ from engine.infer import predict_from_config
 
 def main():
     cfg = TrainConfig(
-        model_name="resunet_pseudo3d",
+        model_name="resunet_pseudo3d_multilevel",
         base=32,
         device="cuda",
         also_save_float32=True,
         snr_sig_stat="p99.99",  # change to e.g. "p95" to use percentile statistic
     )
 
-    run = "s008_g025_loss=charb+gradL1"
+    run = r"sweep_subwindows=8\s004_g050"
     path =r"runs\6mm_1024Aline_strip\\" + run + r"\\checkpoints\best.pt"
     outdir1=r"runs\6mm_1024Aline_strip\\" + run + r"\\predictions\train"
     outdir2=r"runs\6mm_1024Aline_strip\\" + run + r"\\predictions\test"
@@ -23,8 +23,10 @@ def main():
         alines=1024,
         crop_depth=(0, 1024),
         dispersion=[1.315892282e-06, 5.459678905e-10],
-        window_sigma=0.08,
-        gap=0.25,
+        window_sigma=0.04,
+        gap=0.50,
+        n_sub_windows=8,            # 0=disabled; e.g. 8 sub-windows per parent (16 total)
+        sub_window_spread=2.0,    # sub-window center spread in sigma units
     )
 
     predict_from_config(
@@ -41,8 +43,10 @@ def main():
         alines=2048,
         crop_depth=(0, 1024),
         dispersion=[4.778474717e-06, 6.475358372e-09],
-        window_sigma=0.08,
-        gap=0.25,
+        window_sigma=0.04,
+        gap=0.50,
+        n_sub_windows=8,            # 0=disabled; e.g. 8 sub-windows per parent (16 total)
+        sub_window_spread=2.0,    # sub-window center spread in sigma units
     )
 
     predict_from_config(
